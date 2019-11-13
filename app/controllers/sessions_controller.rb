db48b98ce81d19f1ae8e0436e0ad30493ccfb2e2
Michelle Wong <michellekwong2@gmail.com>
2019-11-13 14:31:59 -0500

    perfect but ugly looking

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,7 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(username: params[:username])
-        # if session[:store_name]
-        #     @store = Store.create(name: session[:store_name], location: session[:location], user_id: user.id)
-        #     if @store.erorrs
-        #         flash[:errors] = @store.errors.full_messages
-              
-        #         redirect_to add_store_path
-        #     end
-        # end
-
+      
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect_to home_path
